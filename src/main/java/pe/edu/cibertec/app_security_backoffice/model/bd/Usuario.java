adafe62b76caf3774ea6de0551cdd550c5fa5361
ricardojavierah <ricardojavierah@gmail.com>
2024-06-07 21:22:59 -0500

    bd

---
+++ src/main/java/pe/edu/cibertec/app_security_backoffice/model/bd/Usuario.java
@@ -0,0 +1,27 @@
+package pe.edu.cibertec.app_security_backoffice.model.bd;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Set;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "usuario")
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idusuario;
+    private String nomusuario;
+    private String email;
+    private String password;
+    private String nombres;
+    private String apellidos;
+    private Boolean activo;
+    @ManyToMany(cascade = CascadeType.MERGE, fetch = FetchType.EAGER)
+    @JoinTable(name = "usuario_rol", joinColumns = @JoinColumn(name = "idusuario"),
+            inverseJoinColumns = @JoinColumn(name = "idrol"))
+    private Set<Rol> roles;
+}
